67d5985723c3c66282434028e21d10032d515325
xushijun5210 <2273525664@qq.com>
2024-04-16 10:17:42 +0800

    11
    实践非对称加密 RSA（编程语言不限）：
    
    先生成一个公私钥对
    用私钥对符合 POW 4个开头的哈希值的 “昵称 + nonce” 进行私钥签名
    用公钥验证

---
+++ key.py
@@ -0,0 +1,32 @@
+
+import hashlib
+
+import rsa
+
+def getSHA256Str(string, num):
+    nonce1 = 0
+    while True:
+        nonce1 = nonce1 + 1
+        str2 = string + str(nonce1)
+        hash_value = hashlib.sha256(str2.encode("utf-8")).hexdigest()
+        if hash_value.startswith('0' * num):
+            return nonce1
+def main():
+    num = getSHA256Str('junge', 4)
+    # 生成密钥对
+    (public_key, private_key) = rsa.newkeys(1024)
+
+    # 加密
+    message = 'junge' + str(num)
+    encrypted_message = rsa.encrypt(message.encode(), public_key)
+
+    # 解密
+    decrypted_message = rsa.decrypt(encrypted_message, private_key).decode()
+
+    print(decrypted_message)  # 输出 message = 'junge' + str(num)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
+
