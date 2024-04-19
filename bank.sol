21c9dc60cfd794b28f36bae52c691e5e4a5cd1fd
xushijun5210 <2273525664@qq.com>
2024-04-19 14:50:03 +0800

    优化bank

--- bank.sol
+++ bank.sol
@@ -5,38 +5,47 @@ contract Bank {
     //2.在 Bank 合约你几率每个地址的存款金额
     //3.编写 withdraw() 方法，仅管理员可以通过该方法提取资金。
     //4.用数组记录存款金额的前 3 名用户
-    mapping(address => uint) deposits; // 存储每个地址的存款金额
+    mapping(address => uint) public deposits; // 存储每个地址的存款金额
     address admin; // 管理员地址
-    address[3] public  top3Depositors;
-    uint[3] public top3Deposits;
-
+    address[] public topThreeAdress;// 存储前3个地址的存款金额
+    uint[3] public topThreeDeposits;
+    address[3] public  topThreeAddr;
+      // 构造函数，设置管理员地址
     constructor() {
         admin = msg.sender;
     }
-    // 通过 Metamask 等钱包直接存款
-    function deposit() payable public {
-        deposits[msg.sender] += msg.value; 
-        updateTop3Depositors(msg.sender, msg.value);
+    // 存款函数，允许用户通过 Metamask 等钱包直接向合约地址存款
+     //通过 Metamask 等钱包直接给 Bank 合约地址存款
+    receive() external payable {
+        require(msg.value>0,"deposit amount must > 0");
+        deposits[msg.sender] += msg.value;
+        updateSortDeposits(msg.sender, msg.value);
     }
    //仅管理员可以通过该方法提取资金。
     function withdraw(uint amount) public {
         require(admin==msg.sender,"only admin can withdraw");
+        //金额不够
         require(admin.balance>=amount,"amount not enouth");
-        payable(admin).transfer(amount);
+        payable (address(this)).transfer(amount);
     }
-     // 按存款金额排序的函数deposits
-    //更新前3列表
-    function updateTop3Depositors(address depositor,uint amount) internal{
-        for (uint i=0; i<top3Deposits.length; i++) {
-            if (amount > top3Deposits[i]) {
-                for (uint j = top3Deposits.length - 1; j > i; j--) {
-                    top3Depositors[j] = top3Depositors[j-1];
-                    top3Deposits[j] = top3Deposits[j-1];
+    //对存储每个地址的存款金额大小进行排序
+    function updateSortDeposits(address depositor,uint amount) internal{
+        for (uint i=0; i<topThreeDeposits.length; i++) {
+            if (amount > topThreeDeposits[i]) {
+                for (uint j = topThreeDeposits.length - 1; j > i; j--) {
+                    topThreeDeposits[j] = topThreeDeposits[j-1];
+                    topThreeDeposits[j] = topThreeDeposits[j-1];
+        for(uint i=0;i<topThreeAddr.length;i++){
+            if (amount > deposits[topThreeAddr[i]]){
+                for (uint j=topThreeAddr.length-1; j>i; j--) {
+                    topThreeAddr[j] = topThreeAddr[j-1];
                 }
-                top3Depositors[i] = depositor;
-                top3Deposits[i] = amount;
+                topThreeDeposits[i] = depositor;
+                topThreeDeposits[i] = amount;
+                topThreeAddr[i] = depositor;
                 break ;
             }
         }
     }
+
 }
